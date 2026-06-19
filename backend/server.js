79ec73d468dd645612d630af4daf08bf27b9e705
Aayushi Mathur <aayushimathur5071@gmail.com>
2026-06-19 17:22:11 +0530

    Fix MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ app.use(express.json());
 
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/aura_db";
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
     console.log("🌐 MongoDB Status: Connected Successfully");
-}).catch((err) => {
+})
+.catch((err) => {
     console.error("⚠️ MongoDB Connection Error:", err);
 });
 
